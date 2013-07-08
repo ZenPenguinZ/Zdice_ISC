2bbc80a53f3d63326e890937774a477178d1b916
David Foster <dbfoster@gmail.com>
2013-07-08 12:58:36 -0700

    inital commit

---
+++ Dice.py
@@ -0,0 +1,704 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pygtk
+pygtk.require('2.0')
+import gobject
+import random
+import os
+import sys
+import gtk
+from zdice import Die
+from zdice import Hand
+from zdice import Player
+from zdice import Players_Group
+from zdice import Dice_Cup
+#EndImports
+
+class iscApp1:
+ iscVerror = "Error!"
+ iscVred = "red"
+ iscVyellow = "yellow"
+ iscVdie2 = "die2"
+ iscVdie1 = "die1"
+ iscVgreen = "green"
+ iscVdie1text = ""
+ iscVdie2text = ""
+ iscVisGreenBrains = "Green brains"
+ iscVisYellowbrains = "Yellow brains"
+ iscVisRedbrains = "Red brains"
+ iscVisGreenrun = "Green run"
+ iscVisYellowrun = "Yellow run"
+ iscVisYellowshot = "Yellow shot"
+ iscVisRedrun = "Red run"
+ iscVisGreenshot = "Green shot"
+ iscVisRedshot = "Red shot"
+ iscVDice_Cup = ""
+ iscVPlayers_Group = ""
+ iscVhand = ""
+ iscVdie0text = ""
+ iscVzero = 0
+ iscVone = 1
+ iscVtwo = 2
+ iscWindow4Window1 = gtk.Window(gtk.WINDOW_TOPLEVEL)
+ iscWindow4Window1Fixed = gtk.Fixed()
+ iscWindow4Die10 = gtk.Image()
+ iscWindow4Die20 = gtk.Image()
+ iscWindow4Roll0 = gtk.Button("Roll")
+ iscWindow4test10 = gtk.Entry()
+ iscWindow4test20 = gtk.Entry()
+ iscWindow4die00 = gtk.Image()
+ iscWindow4Label0 =gtk.Label("Hand")
+ iscWindow4Test00 = gtk.Entry()
+
+ #EndOfGlobalVariables
+
+ def main(self):
+  gtk.main()
+
+ def destroy(self, widget, data=None):
+  gtk.main_quit()
+
+
+
+#EndOfClass
+
+def iscSetTextBox1():
+ thisiscApp1.iscWindow4test20.set_text(thisiscApp1.iscVdie2text)
+ #iscSetTextBox1Done
+
+
+def iscSetTextBox2():
+ thisiscApp1.iscWindow4test10.set_text(thisiscApp1.iscVdie1text)
+ #iscSetTextBox2Done
+
+
+def iscSetTextBox3():
+ thisiscApp1.iscWindow4Test00.set_text(thisiscApp1.iscVdie0text)
+ #iscSetTextBox3Done
+
+
+def iscWindow4():
+ thisiscApp1.iscWindow4Die10 = gtk.Image()
+ thisiscApp1.iscWindow4Die20 = gtk.Image()
+ thisiscApp1.iscWindow4Roll0 = gtk.Button("Roll")
+ thisiscApp1.iscWindow4test10 = gtk.Entry()
+ thisiscApp1.iscWindow4test20 = gtk.Entry()
+ thisiscApp1.iscWindow4die00 = gtk.Image()
+ thisiscApp1.iscWindow4Label0 =gtk.Label("Hand")
+ thisiscApp1.iscWindow4Test00 = gtk.Entry()
+ thisiscApp1.iscWindow4Window1 = gtk.Window(gtk.WINDOW_TOPLEVEL)
+ thisiscApp1.iscWindow4Window1Fixed = gtk.Fixed()
+ thisiscApp1.iscWindow4Window1.set_title("Window")
+ thisiscApp1.iscWindow4Window1.set_default_size(480, 640)
+ thisiscApp1.iscWindow4Window1.add(thisiscApp1.iscWindow4Window1Fixed)
+ thisiscApp1.iscWindow4Window1Fixed.width = 480
+ thisiscApp1.iscWindow4Window1Fixed.height = 640
+ thisiscApp1.iscWindow4Window1.connect("delete_event", iscWindow4Closed)
+ thisiscApp1.iscWindow4Window1.set_resizable(False)
+ thisiscApp1.iscWindow4Window1Fixed.show()
+ iscWindow4Die10EventBox = gtk.EventBox()
+ iscWindow4Die10EventBox.set_size_request(60, 60)
+ iscWindow4Die10EventBox.connect("button_press_event", iscWindow4Die1Clicked)
+ thisiscApp1.iscWindow4Die10.set_size_request(60, 60)
+ iscWindow4Die10EventBox.add(thisiscApp1.iscWindow4Die10)
+ thisiscApp1.iscWindow4Window1Fixed.put(iscWindow4Die10EventBox, 140, 150)
+ thisiscApp1.iscWindow4Die10.show()
+ iscWindow4Die10EventBox.show()
+ iscWindow4Die20EventBox = gtk.EventBox()
+ iscWindow4Die20EventBox.set_size_request(60, 60)
+ iscWindow4Die20EventBox.connect("button_press_event", iscWindow4Die2Clicked)
+ thisiscApp1.iscWindow4Die20.set_size_request(60, 60)
+ iscWindow4Die20EventBox.add(thisiscApp1.iscWindow4Die20)
+ thisiscApp1.iscWindow4Window1Fixed.put(iscWindow4Die20EventBox, 230, 150)
+ thisiscApp1.iscWindow4Die20.show()
+ iscWindow4Die20EventBox.show()
+ thisiscApp1.iscWindow4Window1Fixed.put(thisiscApp1.iscWindow4Roll0, 125, 280)
+ thisiscApp1.iscWindow4Roll0.set_size_request(80, 26)
+ thisiscApp1.iscWindow4Roll0.connect("clicked", iscWindow4RollClicked)
+ thisiscApp1.iscWindow4Roll0.show()
+ thisiscApp1.iscWindow4Window1Fixed.put(thisiscApp1.iscWindow4test10, 228, 249)
+ thisiscApp1.iscWindow4test10.set_text("test box")
+ thisiscApp1.iscWindow4test10.set_size_request(132, 27)
+ thisiscApp1.iscWindow4test10.connect("changed", iscWindow4test1Changed)
+ thisiscApp1.iscWindow4test10.show()
+ thisiscApp1.iscWindow4Window1Fixed.put(thisiscApp1.iscWindow4test20, 231, 282)
+ thisiscApp1.iscWindow4test20.set_text("TextBox")
+ thisiscApp1.iscWindow4test20.set_size_request(132, 25)
+ thisiscApp1.iscWindow4test20.connect("changed", iscWindow4test2Changed)
+ thisiscApp1.iscWindow4test20.show()
+ iscWindow4die00EventBox = gtk.EventBox()
+ iscWindow4die00EventBox.set_size_request(60, 60)
+ iscWindow4die00EventBox.connect("button_press_event", iscWindow4die0Clicked)
+ thisiscApp1.iscWindow4die00.set_size_request(60, 60)
+ iscWindow4die00EventBox.add(thisiscApp1.iscWindow4die00)
+ thisiscApp1.iscWindow4Window1Fixed.put(iscWindow4die00EventBox, 50, 150)
+ thisiscApp1.iscWindow4die00.show()
+ iscWindow4die00EventBox.show()
+ thisiscApp1.iscWindow4Window1Fixed.put(thisiscApp1.iscWindow4Label0, 145, 119)
+ thisiscApp1.iscWindow4Label0.set_size_request(45, 18)
+ thisiscApp1.iscWindow4Label0.show()
+ thisiscApp1.iscWindow4Window1Fixed.put(thisiscApp1.iscWindow4Test00, 228, 219)
+ thisiscApp1.iscWindow4Test00.set_text("Textbox0")
+ thisiscApp1.iscWindow4Test00.set_size_request(130, 25)
+ thisiscApp1.iscWindow4Test00.connect("changed", iscWindow4Test0Changed)
+ thisiscApp1.iscWindow4Test00.show()
+ thisiscApp1.iscWindow4Window1.show()
+ iscinit_zdice34()
+ #iscWindow4Opened
+ #iscWindow4Done
+
+
+def iscWindow4Closed(self, other):
+ pass
+ iscAppQuit30()
+ #iscWindow4Closed
+
+
+def iscWindow4Die1Clicked(widget, event):
+ pass
+ #iscWindow4Die1Clicked
+
+
+def iscWindow4Die2Clicked(widget, event):
+ pass
+ #iscWindow4Die2Clicked
+
+
+def iscWindow4RollClicked(self):
+ pass
+ iscRoll_Hand14()
+ #iscWindow4RollClicked
+
+
+def iscWindow4test1Changed(self):
+ pass
+ #iscWindow4test1Changed
+
+
+def iscWindow4test2Changed(self):
+ pass
+ #iscWindow4test2Changed
+
+
+def iscWindow4die0Clicked(widget, event):
+ pass
+ #iscWindow4die0Clicked
+
+
+def iscWindow4Test0Changed(self):
+ pass
+ #iscWindow4Test0Changed
+
+
+def iscGetDiceText6():
+ d = thisiscApp1.iscVhand.dice[thisiscApp1.iscVtwo]
+ thisiscApp1.iscVdie2text = d.__repr__()
+ iscIfThen35()
+ iscSetTextBox1()
+ #iscGetDiceText6Done
+
+def iscGetDiceText8():
+ d = thisiscApp1.iscVhand.dice[thisiscApp1.iscVone]
+ thisiscApp1.iscVdie1text = d.__repr__()
+ iscIfThen22()
+ iscGetDiceText6()
+ iscSetTextBox2()
+ #iscGetDiceText8Done
+
+def iscPortalDestination9():
+ iscGetDiceText11()
+ #iscPortalDestination9Arrived
+
+
+def iscGetDiceText11():
+ d = thisiscApp1.iscVhand.dice[thisiscApp1.iscVzero]
+ thisiscApp1.iscVdie0text = d.__repr__()
+ iscIfThen29()
+ iscGetDiceText8()
+ iscSetTextBox3()
+ #iscGetDiceText11Done
+
+def iscPortalDeparture12():
+ iscPortalDestination9()
+ #iscPortalDeparture12Done
+
+
+def iscRoll_Hand14():
+ thisiscApp1.iscVhand.roll()
+ iscPortalDeparture12()
+ #iscRoll_Hand14Rolled
+
+def iscIfThen15():
+ if thisiscApp1.iscVdie2text == thisiscApp1.iscVisRedrun:
+  iscSetCanvasPicture16()
+  #iscIfThen15True
+
+  pass
+ else:
+  iscSetLabel77()
+  #iscIfThen15False
+
+  pass
+
+def iscSetCanvasPicture16():
+ thisiscApp1.iscWindow4Die20.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/rfeet.jpg") #iscSetCanvasPicture16Done
+
+
+def iscIfThen17():
+ if thisiscApp1.iscVdie2text == thisiscApp1.iscVisRedshot:
+  iscSetCanvasPicture18()
+  #iscIfThen17True
+
+  pass
+ else:
+  iscIfThen15()
+  #iscIfThen17False
+
+  pass
+
+def iscSetCanvasPicture18():
+ thisiscApp1.iscWindow4Die20.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/rshot.jpg") #iscSetCanvasPicture18Done
+
+
+def iscIfThen19():
+ if thisiscApp1.iscVdie1text == thisiscApp1.iscVisRedrun:
+  iscSetCanvasPicture62()
+  #iscIfThen19True
+
+  pass
+ else:
+  iscIfThen54()
+  #iscIfThen19False
+
+  pass
+
+def iscIfThen20():
+ if thisiscApp1.iscVdie0text == thisiscApp1.iscVisRedrun:
+  iscSetCanvasPicture45()
+  #iscIfThen20True
+
+  pass
+ else:
+  iscIfThen21()
+  #iscIfThen20False
+
+  pass
+
+def iscIfThen21():
+ if thisiscApp1.iscVdie0text == thisiscApp1.iscVisRedshot:
+  iscSetCanvasPicture47()
+  #iscIfThen21True
+
+  pass
+ else:
+  iscSetLabel77()
+  #iscIfThen21False
+
+  pass
+
+def iscIfThen22():
+ if thisiscApp1.iscVdie1text == thisiscApp1.iscVisGreenBrains:
+  iscSetCanvasPicture55()
+  #iscIfThen22True
+
+  pass
+ else:
+  iscIfThen48()
+  #iscIfThen22False
+
+  pass
+
+def iscIfThen23():
+ if thisiscApp1.iscVdie0text == thisiscApp1.iscVisRedbrains:
+  iscSetCanvasPicture46()
+  #iscIfThen23True
+
+  pass
+ else:
+  iscIfThen20()
+  #iscIfThen23False
+
+  pass
+
+def iscIfThen24():
+ if thisiscApp1.iscVdie0text == thisiscApp1.iscVisYellowshot:
+  iscSetCanvasPicture44()
+  #iscIfThen24True
+
+  pass
+ else:
+  iscIfThen23()
+  #iscIfThen24False
+
+  pass
+
+def iscIfThen25():
+ if thisiscApp1.iscVdie0text == thisiscApp1.iscVisYellowrun:
+  iscSetCanvasPicture43()
+  #iscIfThen25True
+
+  pass
+ else:
+  iscIfThen24()
+  #iscIfThen25False
+
+  pass
+
+def iscIfThen26():
+ if thisiscApp1.iscVdie0text == thisiscApp1.iscVisYellowbrains:
+  iscSetCanvasPicture42()
+  #iscIfThen26True
+
+  pass
+ else:
+  iscIfThen25()
+  #iscIfThen26False
+
+  pass
+
+def iscIfThen27():
+ if thisiscApp1.iscVdie0text == thisiscApp1.iscVisGreenshot:
+  iscSetCanvasPicture41()
+  #iscIfThen27True
+
+  pass
+ else:
+  iscIfThen26()
+  #iscIfThen27False
+
+  pass
+
+def iscIfThen28():
+ if thisiscApp1.iscVdie0text == thisiscApp1.iscVisGreenrun:
+  iscSetCanvasPicture40()
+  #iscIfThen28True
+
+  pass
+ else:
+  iscIfThen27()
+  #iscIfThen28False
+
+  pass
+
+def iscIfThen29():
+ if thisiscApp1.iscVdie0text == thisiscApp1.iscVisGreenBrains:
+  iscSetCanvasPicture39()
+  #iscIfThen29True
+
+  pass
+ else:
+  iscIfThen28()
+  #iscIfThen29False
+
+  pass
+
+def iscAppQuit30():
+ thisiscApp1.destroy(None,None)
+ #iscAppQuit30Done
+
+
+def iscGet_Hand32():
+ thisiscApp1.iscVhand.fill(thisiscApp1.iscVDice_Cup)
+ iscRoll_Hand37()
+ #iscGet_Hand32Done
+
+def iscinit_zdice34():
+ thisiscApp1.iscVDice_Cup=Dice_Cup()
+ thisiscApp1.iscVPlayers_Group=Players_Group()
+ thisiscApp1.iscVhand=Hand()
+
+ iscGet_Hand32()
+ #iscinit_zdice34Done
+def iscIfThen35():
+ if thisiscApp1.iscVdie2text == thisiscApp1.iscVisGreenBrains:
+  iscSetCanvasPicture70()
+  #iscIfThen35True
+
+  pass
+ else:
+  iscIfThen69()
+  #iscIfThen35False
+
+  pass
+
+def iscRoll_Hand37():
+ thisiscApp1.iscVhand.roll()
+ iscPortalDeparture38()
+ #iscRoll_Hand37Rolled
+
+def iscPortalDeparture38():
+ iscPortalDestination9()
+ #iscPortalDeparture38Done
+
+
+def iscSetCanvasPicture39():
+ thisiscApp1.iscWindow4die00.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/gbrain.jpg") #iscSetCanvasPicture39Done
+
+
+def iscSetCanvasPicture40():
+ thisiscApp1.iscWindow4die00.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/gfeet.jpg") #iscSetCanvasPicture40Done
+
+
+def iscSetCanvasPicture41():
+ thisiscApp1.iscWindow4die00.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/gshot.jpg") #iscSetCanvasPicture41Done
+
+
+def iscSetCanvasPicture42():
+ thisiscApp1.iscWindow4die00.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/ybrain.jpg") #iscSetCanvasPicture42Done
+
+
+def iscSetCanvasPicture43():
+ thisiscApp1.iscWindow4die00.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/yfeet.jpg") #iscSetCanvasPicture43Done
+
+
+def iscSetCanvasPicture44():
+ thisiscApp1.iscWindow4die00.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/yshot.jpg") #iscSetCanvasPicture44Done
+
+
+def iscSetCanvasPicture45():
+ thisiscApp1.iscWindow4die00.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/rfeet.jpg") #iscSetCanvasPicture45Done
+
+
+def iscSetCanvasPicture46():
+ thisiscApp1.iscWindow4die00.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/rbrain.jpg") #iscSetCanvasPicture46Done
+
+
+def iscSetCanvasPicture47():
+ thisiscApp1.iscWindow4die00.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/rshot.jpg") #iscSetCanvasPicture47Done
+
+
+def iscIfThen48():
+ if thisiscApp1.iscVdie1text == thisiscApp1.iscVisGreenrun:
+  iscSetCanvasPicture56()
+  #iscIfThen48True
+
+  pass
+ else:
+  iscIfThen49()
+  #iscIfThen48False
+
+  pass
+
+def iscIfThen49():
+ if thisiscApp1.iscVdie1text == thisiscApp1.iscVisGreenshot:
+  iscSetCanvasPicture57()
+  #iscIfThen49True
+
+  pass
+ else:
+  iscIfThen50()
+  #iscIfThen49False
+
+  pass
+
+def iscIfThen50():
+ if thisiscApp1.iscVdie1text == thisiscApp1.iscVisYellowbrains:
+  iscSetCanvasPicture58()
+  #iscIfThen50True
+
+  pass
+ else:
+  iscIfThen51()
+  #iscIfThen50False
+
+  pass
+
+def iscIfThen51():
+ if thisiscApp1.iscVdie1text == thisiscApp1.iscVisYellowrun:
+  iscSetCanvasPicture59()
+  #iscIfThen51True
+
+  pass
+ else:
+  iscIfThen52()
+  #iscIfThen51False
+
+  pass
+
+def iscIfThen52():
+ if thisiscApp1.iscVdie1text == thisiscApp1.iscVisYellowshot:
+  iscSetCanvasPicture60()
+  #iscIfThen52True
+
+  pass
+ else:
+  iscIfThen53()
+  #iscIfThen52False
+
+  pass
+
+def iscIfThen53():
+ if thisiscApp1.iscVdie1text == thisiscApp1.iscVisRedbrains:
+  iscSetCanvasPicture61()
+  #iscIfThen53True
+
+  pass
+ else:
+  iscIfThen19()
+  #iscIfThen53False
+
+  pass
+
+def iscIfThen54():
+ if thisiscApp1.iscVdie1text == thisiscApp1.iscVisRedshot:
+  iscSetCanvasPicture63()
+  #iscIfThen54True
+
+  pass
+ else:
+  iscSetLabel77()
+  #iscIfThen54False
+
+  pass
+
+def iscSetCanvasPicture55():
+ thisiscApp1.iscWindow4Die10.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/gbrain.jpg") #iscSetCanvasPicture55Done
+
+
+def iscSetCanvasPicture56():
+ thisiscApp1.iscWindow4Die10.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/gfeet.jpg") #iscSetCanvasPicture56Done
+
+
+def iscSetCanvasPicture57():
+ thisiscApp1.iscWindow4Die10.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/gshot.jpg") #iscSetCanvasPicture57Done
+
+
+def iscSetCanvasPicture58():
+ thisiscApp1.iscWindow4Die10.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/ybrain.jpg") #iscSetCanvasPicture58Done
+
+
+def iscSetCanvasPicture59():
+ thisiscApp1.iscWindow4Die10.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/yfeet.jpg") #iscSetCanvasPicture59Done
+
+
+def iscSetCanvasPicture60():
+ thisiscApp1.iscWindow4Die10.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/yshot.jpg") #iscSetCanvasPicture60Done
+
+
+def iscSetCanvasPicture61():
+ thisiscApp1.iscWindow4Die10.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/rbrain.jpg") #iscSetCanvasPicture61Done
+
+
+def iscSetCanvasPicture62():
+ thisiscApp1.iscWindow4Die10.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/rfeet.jpg") #iscSetCanvasPicture62Done
+
+
+def iscSetCanvasPicture63():
+ thisiscApp1.iscWindow4Die10.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/rshot.jpg") #iscSetCanvasPicture63Done
+
+
+def iscIfThen64():
+ if thisiscApp1.iscVdie2text == thisiscApp1.iscVisGreenshot:
+  iscSetCanvasPicture72()
+  #iscIfThen64True
+
+  pass
+ else:
+  iscIfThen65()
+  #iscIfThen64False
+
+  pass
+
+def iscIfThen65():
+ if thisiscApp1.iscVdie2text == thisiscApp1.iscVisYellowbrains:
+  iscSetCanvasPicture73()
+  #iscIfThen65True
+
+  pass
+ else:
+  iscIfThen66()
+  #iscIfThen65False
+
+  pass
+
+def iscIfThen66():
+ if thisiscApp1.iscVdie2text == thisiscApp1.iscVisYellowrun:
+  iscSetCanvasPicture74()
+  #iscIfThen66True
+
+  pass
+ else:
+  iscIfThen67()
+  #iscIfThen66False
+
+  pass
+
+def iscIfThen67():
+ if thisiscApp1.iscVdie2text == thisiscApp1.iscVisYellowshot:
+  iscSetCanvasPicture75()
+  #iscIfThen67True
+
+  pass
+ else:
+  iscIfThen68()
+  #iscIfThen67False
+
+  pass
+
+def iscIfThen68():
+ if thisiscApp1.iscVdie2text == thisiscApp1.iscVisRedbrains:
+  iscSetCanvasPicture76()
+  #iscIfThen68True
+
+  pass
+ else:
+  iscIfThen17()
+  #iscIfThen68False
+
+  pass
+
+def iscIfThen69():
+ if thisiscApp1.iscVdie2text == thisiscApp1.iscVisGreenrun:
+  iscSetCanvasPicture71()
+  #iscIfThen69True
+
+  pass
+ else:
+  iscIfThen64()
+  #iscIfThen69False
+
+  pass
+
+def iscSetCanvasPicture70():
+ thisiscApp1.iscWindow4Die20.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/gbrain.jpg") #iscSetCanvasPicture70Done
+
+
+def iscSetCanvasPicture71():
+ thisiscApp1.iscWindow4Die20.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/gfeet.jpg") #iscSetCanvasPicture71Done
+
+
+def iscSetCanvasPicture72():
+ thisiscApp1.iscWindow4Die20.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/gshot.jpg") #iscSetCanvasPicture72Done
+
+
+def iscSetCanvasPicture73():
+ thisiscApp1.iscWindow4Die20.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/ybrain.jpg") #iscSetCanvasPicture73Done
+
+
+def iscSetCanvasPicture74():
+ thisiscApp1.iscWindow4Die20.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/yfeet.jpg") #iscSetCanvasPicture74Done
+
+
+def iscSetCanvasPicture75():
+ thisiscApp1.iscWindow4Die20.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/yshot.jpg") #iscSetCanvasPicture75Done
+
+
+def iscSetCanvasPicture76():
+ thisiscApp1.iscWindow4Die20.set_from_file(os.path.dirname(sys.argv[0]) + "/ISCimages/rbrain.jpg") #iscSetCanvasPicture76Done
+
+
+def iscSetLabel77():
+ thisiscApp1.iscWindow4Label0.set_label(thisiscApp1.iscVerror)
+ #iscSetLabel77Done
+
+
+#EndOfFunctions
+
+thisiscApp1 = iscApp1()
+
+iscWindow4()
+#iscApp1Launched
+#EndOfStatements
+
+thisiscApp1.main()
---
+++ zdice.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python
+# Filename: zdice.py
+
+from random import shuffle
+from random import randrange
+
+class Player(object):
+    """ defines an individual player"""
+    WINNING_BRAIN_COUNT = 13
+    def __init__(self, name, brain_count = 0):
+        self.name = name
+        self.brain_count = brain_count
+    def __repr__(self):
+        return "player " + self.name + " has " + str(self.brain_count) + " brains."
+    def has_won(self):
+        if self.brain_count >= self.WINNING_BRAIN_COUNT:
+            return True
+        else:
+            return False
+    def bank_brains(self, brains):
+        self.brain_count += brains
+
+
+class Players_Group(object):
+    """Defines the group of players in a round robbin linked list"""
+    player_group = []
+    def __init__(self, player_group=None):
+        if player_group is None:
+            player_group = []
+            self.player_group = player_group
+    def __repr__(self):
+        temp = []
+        for i in self.player_group:
+            temp.append(i.__repr__())
+        return "\n".join(temp)
+    def add_player(self, name):
+        self.player_group.append(Player(name))
+    def shuffle_players(self):
+        shuffle(self.player_group)
+
+
+class Die(object):
+    """takes a color attr and defines a die
+	color is on of "green" "yellow" "red" 
+	Die.side is a tuple of NUMBER_SIDES length made up of action 'words', "brains" "shot" "run"
+	Die.roll_die randomizes the Die.side_up number
+	bool methods for is_color and is_action"""
+
+    NUMBER_SIDES = 6
+    BRAINS = "brains"
+    SHOT = "shot"
+    RUN = "run"
+    GREEN =    (BRAINS,SHOT,RUN,BRAINS,RUN,BRAINS)
+    YELLOW = (BRAINS,RUN,SHOT,BRAINS,RUN,SHOT)
+    RED =         (SHOT,RUN,SHOT,BRAINS,SHOT,RUN)
+    assert len(GREEN) ==  NUMBER_SIDES and  len(RED) ==  NUMBER_SIDES and  len(YELLOW) ==  NUMBER_SIDES
+    def __init__(self, sides , side_up = 0):
+        if sides == "green":
+            self.sides = self.GREEN
+        elif sides == "yellow":
+            self.sides = self.YELLOW
+        elif sides == "red":
+            self.sides = self.RED
+        else:
+            self.sides = "error"
+            print "Failed to init Die"
+        self.side_up = side_up
+    def __repr__(self):
+        if self.is_green():
+            return "Green %s" % self.sides[self.side_up]
+        elif self.is_yellow():
+            return "Yellow %s" % self.sides[self.side_up]
+        elif self.is_red():
+            return "Red %s" % self.sides[self.side_up]
+        else:
+            return "error"
+    def roll_die(self):
+        self.side_up = randrange(self.NUMBER_SIDES)
+#is Color series, returns true if match
+    def is_green(self):
+        return self.sides == self.GREEN
+    def is_yellow(self):
+        return self.sides == self.YELLOW
+    def is_red(self):
+        return self.sides == self.RED
+#is Action series,  returns true if match
+    def is_brains(self):
+        return self.sides[self.side_up] == self.BRAINS
+    def is_shot(self):
+        return self.sides[self.side_up] == self.SHOT
+    def is_run(self):
+        return self.sides[self.side_up] == self.RUN
+
+class Dice_Cup(object):
+    """Cup of dice to draw from"""
+    NUMBER_RED = 3
+    NUMBER_GREEN = 6
+    NUMBER_YELLOW = 4
+    POOL_SIZE = 13
+    cup = []
+    assert NUMBER_RED + NUMBER_GREEN + NUMBER_YELLOW  == POOL_SIZE
+    def __init__(self):
+        for x in range(self.NUMBER_RED):
+             self.cup.append(Die("red"))
+        for x in range(self.NUMBER_GREEN):
+            self.cup.append(Die("green"))
+        for x in range(self.NUMBER_YELLOW):
+            self.cup.append(Die("yellow"))
+        self.shuffle()
+    def __repr__(self):
+        result = []
+        for d in self.cup:
+              result.append(str(d))
+        return "\n ".join(result)
+    def shuffle(self):
+        return shuffle(self.cup)
+    def get_die(self):
+        if len(self.cup) < 1:
+            print "refreshing cup"
+            self.__init__()
+        self.shuffle()
+        return self.cup.pop()
+
+class Hand(object):
+    HAND_SIZE = 3
+    def __init__(self, shots = 0, brains = 0):
+        self.shots = shots
+        self.brains = brains
+        self.dice = []
+    def __repr__(self):
+        result = []
+        for d in self.dice:
+            result.append(d.__repr__())
+        return "|".join(result) + "\n"
+    def fill(self, dice_cup):
+        if len(self.dice) == self.HAND_SIZE:
+            return self.dice
+        for x in range(len(self.dice),self.HAND_SIZE):
+            self.dice.append(dice_cup.get_die())
+    def roll(self):
+        for d in self.dice:
+            d.roll_die()
+    def score(self):
+        for i in range(len(self.dice)):
+            temp_die = self.dice.pop(0)  #pop first die in hand
+            if temp_die.is_brains():
+                self.brains += 1
+            if temp_die.is_shot():
+                self.shots += 1
+                if self.shots >= 3:
+                    return False  #your Dead, turn over
+            if temp_die.is_run():
+                self.dice.append(temp_die) #append back to last die in hand
+        return True #you survived
+        
